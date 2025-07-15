61ae009090f8af3d5f55060053d282d36372e807
nguyenlongdang0412 <nguyenlongdang04121999@gmail.com>
2025-07-15 22:02:34 +0700

    fold header list column

--- features/list/components/ListColumn.tsx
+++ features/list/components/ListColumn.tsx
@@ -2,7 +2,9 @@ import { CSS } from '@dnd-kit/utilities'
 import { CardItemKanban } from '@/features/card'
 import { useSortable } from '@dnd-kit/sortable'
 import { BoardCardsResponse, BoardListsResponse } from '@/types'
-import { CSSProperties, Dispatch, memo, SetStateAction } from 'react'
+import { CSSProperties, Dispatch, memo, SetStateAction, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { FoldHorizontal, UnfoldHorizontal } from 'lucide-react'
 
 interface Props {
     column: BoardListsResponse
@@ -37,27 +39,60 @@ export const ListColumn = memo(function ListColumn({ column, cards, setCards, is
         })
     }
 
+    const [fold, setFold] = useState<boolean>(false)
+
     return (
         <li
             ref={setNodeRef}
             style={style}
-            className="list-none flex-none flex flex-col w-72 bg-white rounded-xl max-h-full pb-3 transition-all duration-200 ease-in-out"
+            className={`list-none flex-none flex flex-col transition-all duration-200 ease-in-out bg-white rounded-xl max-h-full ${fold ? 'w-14 min-w-10 max-w-10 items-center px-0 py-3' : 'w-72 pb-3'}`}
         >
             <header
                 className="cursor-grab active:cursor-grabbing select-none"
                 {...attributes}
                 {...listeners}
             >
-                <div className="flex items-center justify-between px-4 py-3">
-                    <h3>{column.name}</h3>
-                </div>
+                {!fold ? (
+                    <div className="flex items-center justify-between px-4 py-3">
+                        <h3 className="font-semibold">{column.name}</h3>
+
+                        <Button
+                            size="icon"
+                            variant="ghost"
+                            className="cursor-pointer"
+                            onClick={() => setFold(true)}
+                        >
+                            <FoldHorizontal />
+                        </Button>
+                    </div>
+                ) : (
+                    <div className="flex flex-col items-center gap-2 justify-center h-full">
+                        <Button
+                            size="icon"
+                            variant="ghost"
+                            className="cursor-pointer size-4 hover:bg-transparent"
+                            onClick={() => setFold(false)}
+                        >
+                            <UnfoldHorizontal />
+                        </Button>
+
+                        <h3
+                            className="font-semibold whitespace-nowrap origin-bottom-left"
+                            style={{ writingMode: 'vertical-rl' }}
+                        >
+                            {column.name}
+                        </h3>
+                    </div>
+                )}
             </header>
 
-            <CardItemKanban
-                listId={column.id}
-                cards={cards}
-                setCards={setCards}
-            />
+            {!fold && (
+                <CardItemKanban
+                    listId={column.id}
+                    cards={cards}
+                    setCards={setCards}
+                />
+            )}
         </li>
     )
 })
